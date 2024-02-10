59c388c8f2c4b81821e8dd85561f9f7543f17881
Oscardk <dauren2050@yandex.ru>
2024-02-11 00:30:12 +0600

    addviews

--- blogD/views.py
+++ blogD/views.py
@@ -1,11 +1,58 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Post
 from .serializers import PostSerializer
 
-class PostList(generics.ListCreateAPIView):
+class PostList(generics.GenericAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    def get(self, request, *args, **kwargs):
+        """
+        Получение списка всех постов.
+        """
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Создание нового поста.
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostDetail(generics.GenericAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        """
+        Получение информации о конкретном посте.
+        """
+        post = self.get_object()
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        """
+        Обновление информации о конкретном посте.
+        """
+        post = self.get_object()
+        serializer = self.get_serializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Удаление конкретного поста.
+        """
+        post = self.get_object()
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
